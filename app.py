47e86b6e64553ee9a56e34948cebbaf15bbca569
Vaibhav Petkar <vaibhavprakashpetkar@gmail.com>
2026-02-21 13:34:00 +0530

    solve #4

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
     

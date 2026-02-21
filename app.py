483af1f114274d221f15bb7be6c851b0afedf8ca
Vaibhav Petkar <vaibhavprakashpetkar@gmail.com>
2026-02-21 16:20:23 +0530

    created home rout where we can interact with bankend

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 # writting basic flask app code where we retrun "Hello World" when we visit on port 5000
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+
 app = Flask(__name__)
+CORS(app)  # This allows all origins to access the API
+
+# create a route for the home page
 @app.route('/')
-def hello_world():
-    return 'Hello World'
-if __name__ == '__main__':
+def home():
+    return jsonify({"message": "Hello World"})
+
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-    
